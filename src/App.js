187aa65613263051a85b69a0276486e5da413a87
JorgeCabelloR <jorcbello@gmail.com>
2022-05-12 15:57:47 -0400

    desafio5

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Item from "./components/Item/Item";
+import {products} from "./data/productos"
 
 function App() {
+  
+  const task = new Promise ((resolve, reject) => {
+    setTimeout(() => {
+      resolve(products);
+    }, 3000)
+    
+  })
+
+  task.then((result) => {
+    console.log(result)
+  }, err => {
+    console.log(err)
+  }).catch((err) => {
+    console.log(err)
+  })
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {products.map((product, index) => (
+          <Item product={product} key={product.id}/>
+        )
+      )}
     </div>
   );
 }
